33d52c8c50ec21d8fd519cdb28b836c34d1faa47
jamieallen59 <jamieallen59@gmail.com>
2014-08-28 12:17:21 +0100

    updated Gemfile.lock

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,9 @@
 Rails.application.configure do
+
+  config.serve_static_assets = true
+  config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
+  config.assets.compile = true
+  
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
